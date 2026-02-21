e2f62847e592a05c13586cac663f68e1afc373bb
Mehmet Özer Bulut <mobulutnetas@gmail.com>
2026-02-21 23:30:06 +0300

    Applied random loot generation.

--- scripts/loot_generator.gd
+++ scripts/loot_generator.gd
@@ -29,9 +29,73 @@ func _ready() -> void:
 
 func generate_loot() -> Array[Resource]:
 	loot_data.clear()
-	loot_data.append(get_random_weapon())
-	loot_data.append(get_random_armor())
+	
+	var item_count := randi_range(2, 15)
+	
+	for i in item_count:
+		var item := generate_random_item()
+		loot_data.append(item)
+	
 	return loot_data
+
+
+func generate_random_item() -> Resource:
+	if randf() < 0.5:
+		return generate_weapon()
+	else:
+		return generate_armor()
+
+
+func roll_rarity() -> int:
+	var roll := randf()
+
+	if roll < 0.50:
+		return 1
+	elif roll < 0.75:
+		return 2
+	elif roll < 0.90:
+		return 3
+	elif roll < 0.98:
+		return 4
+	else:
+		return 5
+
+
+func generate_weapon() -> Weapon:
+	var base_weapon : Weapon = weapons.pick_random()
+	var weapon : Weapon = base_weapon.duplicate()
+	
+	weapon.rarity = roll_rarity()
+	apply_rarity_scaling(weapon)
+	
+	return weapon
+
+func generate_armor() -> Armor:
+	var base_armor : Armor = armors.pick_random()
+	var armor : Armor = base_armor.duplicate()
+	
+	armor.rarity = roll_rarity()
+	apply_rarity_scaling(armor)
+	
+	return armor
+
+
+func apply_rarity_scaling(item: Resource) -> void:
+	var multiplier := 1.0
+	
+	match item.rarity:
+		1: multiplier = 1.0
+		2: multiplier = 1.15
+		3: multiplier = 1.30
+		4: multiplier = 1.50
+		5: multiplier = 1.80
+	
+	if item is Weapon:
+		item.damage_min *= multiplier
+		item.damage_max *= multiplier
+	
+	elif item is Armor:
+		item.defence *= multiplier
 
 
 func get_random_weapon() -> Weapon:
